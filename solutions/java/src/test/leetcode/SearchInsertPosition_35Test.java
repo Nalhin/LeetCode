44a23c77c2f822e97c330647729a0982ccb23f6f
nalhin <krol.wow@gmail.com>
2020-09-03 22:39:49 +0200

    feat: Search Insert Position (35) and Maximum Subarray (53) solutions.

---
+++ solutions/java/src/test/leetcode/SearchInsertPosition_35Test.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class SearchInsertPosition_35Test {
+
+  SearchInsertPosition_35 solution = new SearchInsertPosition_35();
+
+  private static class TestArgumentsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+
+      return Stream.of(
+          arguments(new int[] {1, 3, 5, 6}, 5, 2),
+          arguments(new int[] {1, 3, 5, 6}, 1, 0),
+          arguments(new int[] {1, 3, 5, 6}, 3, 1),
+          arguments(new int[] {1, 3, 5, 6}, 6, 3),
+          arguments(new int[] {1, 3, 5, 6}, 7, 4),
+          arguments(new int[] {1, 3, 5, 6}, 0, 0));
+    }
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(TestArgumentsProvider.class)
+  void searchInsert(int[] nums, int target, int expected) {
+    int result = solution.searchInsert(nums, target);
+
+    assertThat(result).isEqualTo(expected);
+  }
+}
