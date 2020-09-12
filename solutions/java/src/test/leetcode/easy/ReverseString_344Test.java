c98bf9f498cef062e7f1b234eec0e097b39b009a
nalhin <krol.wow@gmail.com>
2020-09-12 20:57:03 +0200

    feat: ReverseString (344) solution.

---
+++ solutions/java/src/test/leetcode/easy/ReverseString_344Test.java
@@ -0,0 +1,38 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class ReverseString_344Test {
+
+    ReverseString_344 solution = new ReverseString_344();
+
+    private static class TestArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+
+            return Stream.of(
+                    arguments(new char[] {'h','e','l','l','o'}, new char[] {'o','l','l','e','h'}),
+                    arguments(new char[] {'H','a','n','n','a','h'}, new char[] {'h','a','n','n','a','H'}));
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(TestArgumentsProvider.class)
+    void longestCommonPrefix(char[] input, char[] expectedResult) {
+
+        solution.reverseString(input);
+
+        assertThat(input).isEqualTo(expectedResult);
+    }
+}
