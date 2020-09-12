e245bb6de723e5052e8da764c9a6c11983cedefe
nalhin <krol.wow@gmail.com>
2020-09-12 19:58:41 +0200

    feat: SingleNumber (136) solution.

---
+++ solutions/java/src/main/leetcode/easy/SingleNumber_136.java
@@ -0,0 +1,38 @@
+package easy;
+
+// Given a non-empty array of integers, every element appears twice except for on
+// e. Find that single one.
+//
+// Note:
+//
+// Your algorithm should have a linear runtime complexity. Could you implement i
+// t without using extra memory?
+//
+// Example 1:
+//
+//
+// Input: [2,2,1]
+// Output: 1
+//
+//
+// Example 2:
+//
+//
+// Input: [4,1,2,1,2]
+// Output: 4
+//
+// Related Topics Hash Table Bit Manipulation
+// 👍 4872 👎 169
+
+// leetcode submit region begin(Prohibit modification and deletion)
+
+public class SingleNumber_136 {
+  public int singleNumber(int[] nums) {
+    int sum = 0;
+    for (int num : nums) {
+      sum ^= num;
+    }
+    return sum;
+  }
+}
+// leetcode submit region end(Prohibit modification and deletion)
