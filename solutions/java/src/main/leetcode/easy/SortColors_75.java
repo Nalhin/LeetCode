c45c01cb18a069f60d49358775ba92f08092e56a
nalhin <krol.wow@gmail.com>
2020-09-12 22:14:51 +0200

    feat: SortColors (75) solution.

---
+++ solutions/java/src/main/leetcode/easy/SortColors_75.java
@@ -0,0 +1,70 @@
+package easy;
+
+// Given an array nums with n objects colored red, white, or blue, sort them in-p
+// lace so that objects of the same color are adjacent, with the colors in the orde
+// r red, white, and blue.
+//
+// Here, we will use the integers 0, 1, and 2 to represent the color red, white,
+// and blue respectively.
+//
+// Follow up:
+//
+//
+// Could you solve this problem without using the library's sort function?
+// Could you come up with a one-pass algorithm using only O(1) constant space?
+//
+//
+//
+// Example 1:
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+// Example 2:
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+// Example 3:
+// Input: nums = [0]
+// Output: [0]
+// Example 4:
+// Input: nums = [1]
+// Output: [1]
+//
+//
+// Constraints:
+//
+//
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is 0, 1, or 2.
+//
+// Related Topics Array Two Pointers Sort
+// 👍 3929 👎 236
+
+// leetcode submit region begin(Prohibit modification and deletion)
+
+public class SortColors_75 {
+  public void sortColors(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    for (int i = 0; i < nums.length; i++) {
+      while (nums[i] == 2 && right > i) {
+        swap(i, right--, nums);
+      }
+      while (nums[i] == 0 && left < i) {
+        swap(i, left++, nums);
+      }
+    }
+  }
+
+  private void swap(int i1, int i2, int[] nums) {
+    int temp = nums[i1];
+    nums[i1] = nums[i2];
+    nums[i2] = temp;
+  }
+}
+
+/*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Sort Colors.
+    Memory Usage: 37.9 MB, less than 78.66% of Java online submissions for Sort Colors.
+ */
+// leetcode submit region end(Prohibit modification and deletion)
--- solutions/java/src/test/leetcode/easy/SortColors_75Test.java
+++ solutions/java/src/test/leetcode/easy/SortColors_75Test.java
@@ -11,23 +11,29 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-class SingleNumber_136Test {
+class SortColors_75Test {
 
-  SingleNumber_136 solution = new SingleNumber_136();
+  SortColors_75 solution = new SortColors_75();
 
   private static class TestArgumentsProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
 
-      return Stream.of(arguments(new int[] {2, 2, 1}, 1), arguments(new int[] {4, 1, 2, 1, 2}, 4));
+      return Stream.of(
+          arguments(new int[] {2, 0, 2, 1, 1, 0}, new int[] {0, 0, 1, 1, 2, 2}),
+          arguments(new int[] {1, 0}, new int[] {0, 1}),
+          arguments(new int[] {1, 2, 0}, new int[] {0, 1, 2}),
+          arguments(new int[] {2, 2, 2, 1, 1, 0}, new int[] {0, 1, 1, 2, 2, 2}),
+          arguments(new int[] {2, 0, 2, 1, 1, 0}, new int[] {0, 0, 1, 1, 2, 2}),
+          arguments(new int[] {2, 0, 1}, new int[] {0, 1, 2}));
     }
   }
 
   @ParameterizedTest
   @ArgumentsSource(TestArgumentsProvider.class)
-  void singleNumber(int[] nums, int expectedResult) {
-    int actualResult = solution.singleNumber(nums);
+  void sortColors(int[] input, int[] expectedResult) {
+    solution.sortColors(input);
 
-    assertThat(actualResult).isEqualTo(expectedResult);
+    assertThat(input).isEqualTo(expectedResult);
   }
 }
